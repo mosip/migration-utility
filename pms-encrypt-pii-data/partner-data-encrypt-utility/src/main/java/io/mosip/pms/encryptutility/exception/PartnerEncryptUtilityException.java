abebed9b05c3984b2945fe1dfe92dc80c1a2c8ca
sudeep <sudeep.j7353@gmail.com>
2025-07-17 16:32:06 +0530

    MOSIP-41491 : Security Testing: PMS Legacy - DB has PII issues - Create utility to encrypt existing data in DB
    Signed-off-by: sudeep <sudeep.j7353@gmail.com>

--- pms-encrypt-pii-data/partner-data-encrypt-utility/src/main/java/io/mosip/pms/encryptutility/exception/PartnerEncryptUtilityException.java
+++ pms-encrypt-pii-data/partner-data-encrypt-utility/src/main/java/io/mosip/pms/encryptutility/exception/PartnerEncryptUtilityException.java
@@ -2,11 +2,6 @@ package io.mosip.pms.encryptutility.exception;
 
 import io.mosip.kernel.core.exception.BaseUncheckedException;
 
-/**
- * @author Nagarjuna
- *
- */
-
 public class PartnerEncryptUtilityException extends BaseUncheckedException {
 
     private static final long serialVersionUID = 1L;
